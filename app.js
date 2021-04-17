06fa3a1cb715c8bd16293fae25d13c230379b325
AndresGallop <andresgallod.i@gmail.com>
2021-04-17 00:03:12 -0400

    this

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ hbs.partials = hbs.templates; // this makes Vue part s work in hbs partials
 
 const port = process.env.PORT || 6060;
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public'))); //Esta linea busca los archivos como css, imagenes.
